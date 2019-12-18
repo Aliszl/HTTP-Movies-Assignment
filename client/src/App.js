a039b2b5f835a53700d5677f86116579b4f9cd61
Alison Law <alisparklaw@gmail.com>
2019-12-18 18:24:08 +0000

    progress so far

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,18 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import UpdatedMovie from "./Movies/UpdatedMovie"
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [currentMovieId, setCurrentMovieId] = useState(null);
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
+  };
+
+  const MovieAction = movie => {
+    setCurrentMovieId([...currentMovieId, movie]);
   };
 
   return (
